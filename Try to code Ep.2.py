a18b03c0939a2ed4e43eea6e091f307dbdc81644
HRZXD <98503935+HRZXD@users.noreply.github.com>
2022-06-14 22:54:52 +0700

    Update Try to code Ep.2.py

--- Try to code Ep.2.py
+++ Try to code Ep.2.py
@@ -1,10 +1,15 @@
-food_list = str(fried rice , omelet, x ,y)
-main_course_list = []
-for food in food_list(4):
-    if food.type == 'main':
-        main_course_list.append(food)
+food_list = ["x","y","z","n","m",'p']
+food = 4
+q = quality_score = [7,4,10,8]
+r = popularity_score = [5,9,3,5]
+main_course_list = ["fried","ome","kua","chick"]
+main = main_course_list
+for food in food_list:
+    if type(main) == str:
+        main_course_list.append("kua")
+
 for food in main_course_list:
-    q=food.quality_score
-    r=food.popularity_score
-    s=(0.6 * q) + (0.4 * r)
-    food.ranking_score = s
+    q=2
+    r=2
+    s= (0.6 * q) + (0.4 * r)
+
